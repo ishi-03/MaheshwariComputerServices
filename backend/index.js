9f3bedf051980ce59b1ffd028f702dae9ad71298
ishi-03 <ishursmd@gmail.com>
2025-08-03 22:16:37 +0530

    Reorganized full project structure

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@
 import dotenv from "dotenv";
 
 dotenv.config();
+console.log("MONGO_URI:", process.env.MONGO_URI);
+
 import path from "path";
 
 import express from "express";
@@ -46,8 +48,15 @@ app.use("/api/config", configRoutes);
 
 
 
-
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client", "dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"))
+  );
+}
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
---
+++ client/src/redux/store.js
@@ -0,0 +1,30 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query/react";
+import { apiSlice } from "./api/apiSlice.js";
+import authReducer from "./features/auth/authSlice.js";
+import favoritesReducer from "./features/favorites/favoriteSlice.js";
+import cartSliceReducer from "./features/cart/cartSlice.js";
+import shopReducer from "./features/shop/shopSlice.js";
+import { getFavoritesFromLocalStorage } from "../Utils/localStorage.js";
+const initialFavorites = getFavoritesFromLocalStorage() || [];
+
+const store = configureStore({
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authReducer,
+    favorites: favoritesReducer,
+    cart: cartSliceReducer,
+    shop: shopReducer,
+  },
+
+  preloadedState: {
+    favorites: initialFavorites,
+  },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: true,
+});
+
+setupListeners(store.dispatch);
+export default store;
