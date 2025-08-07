5f4a90b9e58841fab6f9734788887293ce715490
ishi-03 <ishursmd@gmail.com>
2025-08-07 15:10:10 +0530

    changed constants.js

--- client/src/redux/constants.js
+++ client/src/redux/constants.js
@@ -1,14 +1,5 @@
-// export const BASE_URL = "";
-// export const USERS_URL = "/api/users";
-// export const CATEGORY_URL = "/api/category";
-// export const VENDOR_URL = "/api/vendor";
 
-// export const PRODUCT_URL = "/api/products";
-// export const UPLOAD_URL = "/api/upload";
-
-// export const ORDERS_URL = "/api/orders";
-// export const RAZORPAY_URL = `${ORDERS_URL}/razorpay`;
-export const BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
+export const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const USERS_URL = `${BASE_URL}/api/users`;
 export const CATEGORY_URL = `${BASE_URL}/api/category`;
