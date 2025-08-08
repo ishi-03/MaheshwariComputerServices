64a3883da857d1da8a5713d67386819d4f439e90
ishi-03 <ishursmd@gmail.com>
2025-08-08 12:45:43 +0530

    changed endsWith

--- client/src/redux/constants.js
+++ client/src/redux/constants.js
@@ -1,8 +1,10 @@
 // remove trailing slash if exists
-const rawBaseUrl = import.meta.env.VITE_API_URL;
+const rawBaseUrl = import.meta.env.VITE_API_URL ?? "";
+
 export const BASE_URL = rawBaseUrl.endsWith("/")
   ? rawBaseUrl.slice(0, -1)
   : rawBaseUrl;
+
 
 // build all URLs cleanly
 export const USERS_URL = `${BASE_URL}/api/users`;
