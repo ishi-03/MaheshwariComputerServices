f252d0d4798e861b026ee496adcfb2319cfef5ef
ishi-03 <ishursmd@gmail.com>
2025-08-12 13:25:16 +0530

    added domain

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -4,7 +4,7 @@ const generateToken = (res, userId) => {
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "30d" });
     res.cookie('jwt', token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
+        secure: true,
         sameSite: 'none',
         maxAge: 30 * 24 * 60 * 60 * 1000,
         domain: '.onrender.com',
--- client/src/redux/api/usersApiSlice.js
+++ client/src/redux/api/usersApiSlice.js
@@ -10,7 +10,6 @@ export const userApiSlice = apiSlice.injectEndpoints({
         url: `${USERS_URL}/auth`, 
         method: "POST",
         body: data,
-        credentials: 'include', 
 
       }),
     }),
